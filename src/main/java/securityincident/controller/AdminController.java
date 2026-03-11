a787f3d476407d08b430f9c46146c60c632940f5
leehyeon2021 <leehyeon2021@gmail.com>
2026-03-11 11:42:22 +0900

    260311 11:42

--- src/main/java/securityincident/controller/AdminController.java
+++ src/main/java/securityincident/controller/AdminController.java
@@ -1,6 +1,5 @@
 package securityincident.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ src/main/java/securityincident/security_db.sql
@@ -0,0 +1,2 @@
+DROP database IF EXISTS security_db;
+CREATE DATABASE IF NOT EXISTS security_db;
